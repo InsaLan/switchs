3c5f616aa05512cd2dbe9137928f1750f374e4a4
SkytAsul <skytasul@gmail.com>
2022-12-23 13:01:31 +0100

    Updated 48-ports script
    Added comments, made config file an argument and not a prompt, added possibility to have a single port instead of a range.

--- 48-ports/script.py
+++ 48-ports/script.py
@@ -1,19 +1,25 @@
 import json
+import sys
 
-scriptFile = open(input("Fichier de script: "), "r")
+if len(sys.argv) < 2:
+    print("You must specify the switch configuration file")
+    sys.exit()
 
-scriptData = scriptFile.read()
-
-data = json.loads(scriptData)
+scriptFile = open(sys.argv[1], "r")
+data = json.loads(scriptFile.read())
 
 switch_number = data["switch_number"];
 
+# 1 - we copy all ports datas to a new dictionnary with all port ranges being split at 48
 ports = {}
-
 for element in data["ports"]:
-    separation = element.split("-")
-    premier = int(separation[0])
-    deuxieme = int(separation[1])
+    if "-" in element:
+        separation = element.split("-")
+        premier = int(separation[0])
+        deuxieme = int(separation[1])
+    else:
+        premier = int(element)
+        deuxieme = premier
     
     if premier <= 48 and deuxieme > 48:
         ports["{}-48".format(premier)] = data["ports"][element]
@@ -21,6 +27,7 @@ for element in data["ports"]:
     else:
         ports["{}-{}".format(premier, deuxieme)] = data["ports"][element]
 
+# 2 - we create an array of all existing (untagged AND tagged) VLANs in the configuration
 vlans = []
 for port_range in ports:
     if "untagged" in ports[port_range]:
@@ -32,12 +39,9 @@ for port_range in ports:
     for tagged in tagged_list:
         if tagged not in vlans:
             vlans.append(tagged)
-            
-            
 
-
-print(f"Creating config for switch {switch_number}")
-print(f"Vlans: {vlans}")
+# 3 - we have all required datas, so let's go for creating the config file that will get pushed to the switch
+print(f"Creating temporary config for switch {switch_number} with vlans: {vlans}")
 
 # Début de l'écriture du fichier de config
 output = open("tmpScript.cfg", "w")
@@ -55,8 +59,8 @@ set system login admin super-user enable  password :4eae55fe808cc25a83205b467c65
 """
 )
 
-for el in vlans:
-    output.write("set vlan create {}\n".format(el))
+for vlan in vlans:
+    output.write("set vlan create {}\n".format(vlan))
            
 for port_range in ports:
     separation = port_range.split("-")
@@ -79,7 +83,6 @@ for port_range in ports:
 
 
 # vlan
-
 for port_range in ports:
     separation = port_range.split("-")
     premier = int(separation[0])
@@ -93,10 +96,7 @@ for port_range in ports:
         untagged = ports[port_range]["untagged"]
         for i in range(premier, deuxieme + 1):
             output.write(f"set port vlan {fege}.1.{i} {untagged}\n") 
-            
-            
-  
-        
+
 
 output.write("""
 set snmp community hotlinemontreal securityname hotlinemontreal nonvolatile
@@ -107,4 +107,4 @@ set snmp group public user hotlinemontreal security-model v2c nonvolatile
 
 output.close();
 
-print("Terminé")
+print("Done creating config")

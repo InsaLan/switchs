0451d5cc6bedae13bf8b03cccffcbcbb36efd3f4
Ala Al-Deen Alkhafaji <3akevdev@gmail.com>
2024-02-25 02:15:07 +0100

    add -s argument to select switch by number, also refactor main script file cuz why not

--- switchs.py
+++ switchs.py
@@ -2,36 +2,23 @@ from telnetlib import Telnet
 from enterasys24p import Enterasys24p
 from enterasys48p_config import Enterasys48pConfig
 from procurve24p_config import Procurve24pConfig
-import sys, json
+import json
+import argparse
 
-if len(sys.argv) < 4:
-    print("Syntax: <switch_file> <config_file> <switch_password>")
-    sys.exit()
 
-switchFile = open(sys.argv[1], "r")
-listSwitchs = json.load(switchFile)
-
-configFile = open(sys.argv[2], "r")
-listConfig = json.load(configFile)
-
-for switch in listSwitchs:
-    i = input("Switch {} ({}) : {}, {} \t([g]o/[n]ext/[q]uit)".format(switch["ip"], switch["name"], switch["model"], switch["config"]))
-    if i == "q":
-        break
-    if i != "g":
-        continue
-
+def configure_switch(switch, config, switch_password):
+    print(f"Configuring switch {switch['name']} ({switch['ip']})")
     if str(switch["model"]) == "24p-enterasys":
         with Telnet(switch["ip"]) as tn:
             s = Enterasys24p(tn)
 
-            s.authenticate("admin", sys.argv[3])
+            s.authenticate("admin", switch_password)
             print(f"Connected to switch {switch['name']}")
 
             s.beforeVlan()
             s.activateSnmp("hotlinemontreal")
 
-            ports = listConfig[switch["config"]]["ports"]
+            ports = config["ports"]
             for port, config in [(p, ports[p]) for p in ports]:
                 s.setInterface(port)
                 s.setVlanUntagged(port, config["untagged"])
@@ -46,13 +33,52 @@ for switch in listSwitchs:
             s.saveConfig()
 
     elif switch["model"] == "48p-enterasys":
-        s = Enterasys48pConfig(switch["ip"], sys.argv[3], listConfig[switch["config"]])
+        s = Enterasys48pConfig(switch["ip"], switch_password, config)
         s.configure()
 
     elif switch["model"] == "24p-procurve":
-        s = Procurve24pConfig(switch["ip"], switch["name"], sys.argv[3], switch["model_id"], listConfig[switch["config"]])
+        s = Procurve24pConfig(
+            switch["ip"], switch["name"], switch_password, switch["model_id"], config
+        )
         s.configure()
 
     else:
         print(f"Unsupported switch model \"{switch['model']}\"")
-        continue
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Configure switches")
+
+    parser.add_argument("switch_file", help="File containing the list of switches")
+    parser.add_argument(
+        "config_file", help="File containing the list of configurations"
+    )
+    parser.add_argument("switch_password", help="Password for the switches")
+
+    parser.add_argument("--switch", "-s", help="Switch to configure (by number)")
+
+    args = parser.parse_args()
+
+    with open(args.switch_file, "r") as f:
+        listSwitchs = json.load(f)
+
+    with open(args.config_file, "r") as f:
+        listConfig = json.load(f)
+
+    if args.switch is not None:
+        switch_ip = f"172.16.1.1{args.switch}"
+        switch = list(filter(lambda x: x["ip"] == switch_ip, listSwitchs))[0]
+        config = listConfig[switch["config"]]
+        configure_switch(switch, config, args.switch_password)
+    else:
+        for switch in listSwitchs:
+            i = input(f"Switch {switch['ip']} ({switch['name']}) : {switch['model']}, {switch['config']} \t([g]o/[n]ext/[q]uit)")
+            if i == "q":
+                break
+            if i == "g":
+                configure_switch(switch, listConfig[switch["config"]], args.switchPassword)
+
+
+if __name__ == "__main__":
+    main()

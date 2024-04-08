81b8ee528da478f5ecef6c898ef41dc018fb8411
pixup1 <pixup1@yahoo.com>
2024-04-08 20:53:25 +0200

    Procurve fix

--- procurve24p_config.py
+++ procurve24p_config.py
@@ -145,10 +145,9 @@ def procurve24p_config(switch, config, access_password, new_password):
 
     with Telnet(ip) as tn:
         is_password_set = bypass_dumb_prompts(tn, access_password)
-
-        setpassword(
-            tn, new_password
-        )
+        
+        if (not is_password_set) or new_password != access_password:
+            setpassword(tn, new_password)
 
         print(f"Copying config from {tftp_server_ip}...")
         tn.write(
